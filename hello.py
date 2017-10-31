76787ac5c78da60879ba873c6bbbf86fc66ff088
Ashish Raj <ashishrj.162@gmail.com>
2017-10-31 18:25:35 +0530

    Adding static folder, and a search box in new view named search_page

--- hello.py
+++ hello.py
@@ -10,6 +10,14 @@ def hello_world1(name=None):
     #return "HELLO WORLD! Today is {}\n".format(date)
     return render_template('hello.html', name=name)
 
+@app.route('/temp')
+def show_template():
+    return render_template('layout.html')
+
+@app.route('/search')
+def search_in_doc():
+    return render_template('search_page.html')
+
 if __name__ == "__main__":
     #app.run()
     pass
